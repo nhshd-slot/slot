e9837d57e211adf6adc74321fbaa2eec338101ef
Matt Stibbs <git@stibbsy.co.uk>
2015-09-12 16:18:44 +0100

    Fix field name issue

--- sms_generator.py
+++ sms_generator.py
@@ -3,7 +3,7 @@ def new_procedure_message(procedure, location, duration, doctor):
     message = str.format("{0} is available on {1}. Attend the ward in {2} and meet {3} in the junior doctors' office. "
                          "To accept this opportunity reply with {4}",
                          procedure,
-                         ward,
+                         location,
                          duration,
                          doctor,
                          unique_reference)
@@ -15,7 +15,7 @@ def success_response_message(procedure, location, duration, doctor):
     message = str.format("Please attend {0} in {1} and ask for {2} to complete this supervised "
                          "procedure. This learning opportunity has been reserved exclusively for you, please make "
                          "every effort to attend.",
-                         ward,
+                         location,
                          duration,
                          doctor)
     print(message)
