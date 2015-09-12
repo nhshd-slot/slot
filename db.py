aaabe42a0ace1a9bb5f9b1d02b30e6711c096116
Ian Davies <git@iandavies.org>
2015-09-12 15:49:42 +0100

    Load DB worksheets once on startup.

--- db.py
+++ db.py
@@ -8,16 +8,27 @@ class Creds(object):
     def __init__(self, access_token):
         self.access_token = access_token
 
+
+print "Loading DB..."
+creds = Creds(config.google_access_token)
+gs = gspread.authorize(creds)
+sheet = gs.open_by_key(config.google_sheet_key)
+log_worksheet = sheet.worksheet("log")
+student_worksheet = sheet.worksheet("students")
+
+print "Complete"
+
+
+
 def to_timestamp(dt):
     return (dt - datetime.datetime(1970, 1, 1)).total_seconds()
 
+def get_all_students():
+    return student_worksheet.get_all_records()
+
 def get_all_opportunities():
 
-    c = Creds(config.google_access_token)
-    gs = gspread.authorize(c)
-    s = gs.open_by_key(config.google_sheet_key)
-    w = s.worksheet("log")
-    rs = w.get_all_records()
+    rs = log_worksheet.get_all_records()
 
 
     for r in rs:
