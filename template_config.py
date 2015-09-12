99b6363ade0ca0ab162d1659f0ae3c24d846e85a
Matt Stibbs <git@stibbsy.co.uk>
2015-09-12 15:40:50 +0100

    Update template_config

--- template_config.py
+++ template_config.py
@@ -2,7 +2,9 @@
 debug_mode = True
 
 # Twilio Settings
-twilio_sid = "xxxxxxxxx"
-twilio_token = "xxxxxxxxxx"
+twilio_sid = 'xxxxxxxxx'
+twilio_token = 'xxxxxxxxxx'
 
 # Google Sheets Settings
+google_access_token = 'xxxxxxx'
+google_sheet_key = 'xxxxxxxx'
