2164971788de568ce00d284d641ad12ba019c028
Ian Davies <git@iandavies.org>
2015-09-13 12:45:12 +0100

    Cast env port to int

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ def complete_procedure():
 
 if __name__ == '__main__':
     app.debug = config.debug_mode
-    port = os.environ.get("PORT", 5000)
+    port = int(os.environ.get("PORT", 5000))
     print(str.format("Debug Mode is: {0}", app.debug))
     app.run(
         host="0.0.0.0",
