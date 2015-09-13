26f08f0f3275c352351f27a36674b65c6e6d171f
Matt Stibbs <git@stibbsy.co.uk>
2015-09-13 10:05:17 +0100

    Add code for automatically assigning message_refs

--- messaging.py
+++ messaging.py
@@ -3,9 +3,38 @@ import sms_twilio
 import db
 import config
 
+list_of_opportunities = []
 
-def broadcast_procedure(procedure, location, duration, doctor):
-    message = sms_generator.new_procedure_message(procedure, location, duration, doctor)
+
+def get_friendly_ref(id):
+    if not list_of_opportunities:
+        new_ref = 1
+        new_opportunity = dict(id=str(id), ref=new_ref)
+        list_of_opportunities.append(new_opportunity)
+        print(str.format("New ref is {0}", new_ref))
+        return new_ref
+
+    else:
+        temp_list = []
+        for opp in list_of_opportunities:
+            temp_list.append(opp['ref'])
+
+        new_ref = max(temp_list) + 1
+        new_opportunity = dict(id=str(id), ref=new_ref)
+        list_of_opportunities.append(new_opportunity)
+
+        print(str.format("New opportunity added {0}", new_opportunity))
+        return new_ref
+
+
+def remove_unique_ref(ref):
+    print(str.format("Removing ref {0}", ref))
+    list_of_opportunities.pop(str(ref), None)
+
+
+def broadcast_procedure(procedure, location, duration, doctor, ref_id):
+    message_ref = get_friendly_ref(ref_id)
+    message = sms_generator.new_procedure_message(procedure, location, duration, doctor, message_ref)
 
     recipients = db.get_all_students()
     print(recipients)
