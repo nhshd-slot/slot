71665f7a5034e56d54f453a41e214580aa2d89ea
Matt Stibbs <git@stibbsy.co.uk>
2015-09-15 08:32:34 +0100

    Print demo mode on app startup

--- app.py
+++ app.py
@@ -150,6 +150,7 @@ if __name__ == '__main__':
     app.debug = config.debug_mode
     port = int(os.environ.get("PORT", 5000))
     print(str.format("Debug Mode is: {0}", app.debug))
+    print(str.format("Demo Mode is: {0}", config.demo_mode))
     app.run(
         host="0.0.0.0",
         port = port
