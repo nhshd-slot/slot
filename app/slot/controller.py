1e5192bc09b7e49fd011228fe5017905d77a1071
Matt Stibbs <git@stibbsy.co.uk>
2016-01-12 00:12:07 +0000

    Refactor auth into separate module

--- app/slot/controller.py
+++ app/slot/controller.py
@@ -7,36 +7,7 @@ import os
 # Local Modules
 from app import app
 import config
-import db_sheets
-import messaging
-
-
-
-from functools import wraps
-from flask import request, Response
-
-
-def check_auth(username, password):
-    """This function is called to check if a username /
-    password combination is valid.
-    """
-    return username == config.website_user and password == config.website_pass
-
-def authenticate():
-    """Sends a 401 response that enables basic auth"""
-    return Response(
-        'Could not verify your access level for that URL.\n'
-        'You have to login with proper credentials', 401,
-        {'WWW-Authenticate': 'Basic realm="Login Required"'})
-
-def requires_auth(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        auth = request.authorization
-        if not auth or not check_auth(auth.username, auth.password):
-            return authenticate()
-        return f(*args, **kwargs)
-    return decorated
+from auth import requires_auth
 
 
 @app.route('/')
