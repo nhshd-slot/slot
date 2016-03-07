b6db16ceb180e7c35896f926139edea5b700b363
Matt Stibbs <git@stibbsy.co.uk>
2016-03-07 21:26:49 +0000

    Import new users blueprint. Import and register flask-login LoginManager to manage logins.

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,4 @@
 import logging
-from flask import Flask
-from flask.ext.cache import Cache
-
-app = Flask(__name__)
-
-app.config.from_object('config')
-
-# Define a Cache for the application and clear it on startup
-cache = Cache(app,config={'CACHE_TYPE': 'redis'})
-
-with app.app_context():
-    cache.clear()
-
-from app.slot import routes, controller
 
 # Set up logging
 log = logging.getLogger('slot')
@@ -20,3 +6,34 @@ log.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 log.addHandler(ch)
+
+from flask import Flask
+from flask.ext.cache import Cache
+from flask.ext.login import LoginManager
+from app.slot.models import User
+
+
+app = Flask(__name__)
+app.config.from_object('config')
+cache = Cache(app,config={'CACHE_TYPE': 'redis'})
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+with app.app_context():
+    cache.clear()
+
+from app.slot import routes, controller
+from app.slot.users.views import users_blueprint
+
+app.register_blueprint(users_blueprint)
+
+from app.slot.models import User
+
+login_manager.login_view = "users.login"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User('slot', 'test')
+
