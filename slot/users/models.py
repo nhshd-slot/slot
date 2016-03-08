0f41d3810c1f7e38b502ee531e1594d4e85e08d7
Matt Stibbs <git@stibbsy.co.uk>
2016-03-08 22:31:59 +0000

    Move models.py to the users blueprint

--- slot/users/models.py
+++ slot/users/models.py
@@ -3,13 +3,12 @@ from flask_login import UserMixin
 
 class User(UserMixin):
     # proxy for a database of users
-    user_database = {"test": ("slot", "test"),
-               "live": ("slot", "live")}
+    user_database = {"test": ("slot", "test"), "live": ("slot", "live")}
 
     def __init__(self, username, password):
         self.id = username
         self.password = password
 
     @classmethod
-    def get(cls,id):
+    def get(cls, id):
         return cls.user_database.get(id)
