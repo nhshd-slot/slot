1f124ca4c13a0e5ceb9796bb1df95e5f31c5bd04
Matt Stibbs <git@stibbsy.co.uk>
2016-03-11 09:03:37 +0000

    Remove redundant parts of User class.

--- slot/users/models.py
+++ slot/users/models.py
@@ -2,13 +2,7 @@ from flask_login import UserMixin
 
 
 class User(UserMixin):
-    # proxy for a database of users
-    user_database = {"test": ("slot", "test"), "live": ("slot", "live")}
 
     def __init__(self, username, password):
         self.id = username
         self.password = password
-
-    @classmethod
-    def get(cls, id):
-        return cls.user_database.get(id)
