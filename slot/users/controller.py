4f9a94be4046af4d805f9f755e7a60076d207832
Matt Stibbs <git@stibbsy.co.uk>
2016-03-11 09:02:18 +0000

    Rename method for validating credentials.

--- slot/users/controller.py
+++ slot/users/controller.py
@@ -1,7 +1,15 @@
-def validate_credentials(username, password):
-    if username == 'slot' and password == 'test':
-        print('Valid credentials')
-        return True
+def return_user_if_valid_credentials(username, password):
+    # Try and retrieve the user from the database using the username - if successful it means the user exists
+    user = db.get_user(username)
+
+    if user:
+        # Check that the password is correct
+        if password == user['password']:
+            print('Valid credentials')
+            user_instance = return_user_instance_or_anonymous(user)
+            if isinstance(user_instance, models.User):
+                return user_instance
+
     else:
         print('Invalid credentials')
-        return False
+        return None
