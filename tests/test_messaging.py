3118ab8122f03b95666d8980004583fe73ab7860
Matt Stibbs <git@stibbsy.co.uk>
2016-03-14 00:24:29 +0000

    Add new test

--- tests/test_messaging.py
+++ tests/test_messaging.py
@@ -8,6 +8,11 @@ class TestMessaging:
         assert isinstance(result, int)
         assert (result == 441234567890)
 
+    def test_converts_mobile_string_with_plus_prefix_to_int(self):
+        result = slot.messaging.mobile_number_string_to_int("+441234567890")
+        assert isinstance(result, int)
+        assert (result == 441234567890)
+
     def test_converts_mobile_int_to_int(self):
         result = slot.messaging.mobile_number_string_to_int(441234567890)
         assert isinstance(result, int)
